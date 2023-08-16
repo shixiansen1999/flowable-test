02458fd34a2d79158bc960db344eba9cb412597d
sbx <26050364543@qq.com>
2023-08-16 17:11:01 +0800

    feat(flowable): 初始

---
+++ src/main/java/com/sbx/App.java
@@ -0,0 +1,13 @@
+package com.sbx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
